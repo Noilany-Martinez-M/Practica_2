ba80a6f43e5903bc350384d0a3ab9a4edd7fd447
Noilany Martinez Mora <maymm.zyn@gmail.com>
2026-02-24 23:46:41 -0600

    codigo final con lo que se pudo implementar

--- practica_programada/src/main/java/com/practica_programada/service/CursoService.java
+++ practica_programada/src/main/java/com/practica_programada/service/CursoService.java
@@ -6,7 +6,6 @@ package com.practica_programada.service;
 import com.practica_programada.domain.Curso;
 import com.practica_programada.repository.CursoRepository;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
